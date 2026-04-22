65cd7a5bf9cbf65b7eacb86aa9d8f9465b1ba0f8
johannajonsson <jojo.risin@outlook.com>
2026-04-22 13:19:45 +0200

    featured scheduled productsync. Add RestTemplateConfig. enhance syncProducts efficacy

--- src/main/java/se/jensen/johanna/fakestoreproductservice/repository/ProductRepository.java
+++ src/main/java/se/jensen/johanna/fakestoreproductservice/repository/ProductRepository.java
@@ -1,8 +1,10 @@
 package se.jensen.johanna.fakestoreproductservice.repository;
 
+import java.util.Set;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import se.jensen.johanna.fakestoreproductservice.model.Product;
 
@@ -10,5 +12,8 @@ import se.jensen.johanna.fakestoreproductservice.model.Product;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
   Page<Product> findAll(Pageable pageable);
+
+  @Query("SELECT p.id FROM Product p")
+  Set<Long> findAllIds();
 
 }
